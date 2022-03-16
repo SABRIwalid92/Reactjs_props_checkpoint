0e5eda486ab3336bfedfc50f7cfc0aeb65dafbb9
carch921 <walid.sabri@esprit.tn>
2022-03-16 01:59:03 +0100

    first commit

---
+++ src/profile/profile.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+function Profile(props) {
+  const data = props.data;
+  return (
+    <div>
+      <h1 style={{color:"gold", textShadow:'2px 2px grey'}}>
+        <i>i,</i>
+        {data.fullName}
+      </h1>
+      <h2 style={{color:"cadetblue"}}>{data.bio}</h2>
+      <h3 style={{}}>though, {data.profession}</h3>
+      <img src={props.children} />
+      <br />
+      <button onClick={data.handleName(data.fullName)}>Who is this</button>
+    </div>
+  );
+  
+
+     Profile.defaultProps={fullName : "Foulen ben Foulen",
+    bio : "(if only he existed)",
+    profession : "what Foulen does.",handleName:()=>alert('nothing to say')};
+    Profile.propTypes ={
+       fullName:PropTypes.string, 
+       bio:PropTypes.string, 
+       profession:PropTypes.string, 
+       handleName:PropTypes.func
+    }
+
+
+  //
+}
+
+export default Profile;
